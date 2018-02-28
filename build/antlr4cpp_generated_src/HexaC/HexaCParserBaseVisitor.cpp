b5897ba79a755688e41d378a08eae814025771ac
Tristan Bourvon <tristanbourvon@gmail.com>
2018-02-28 15:12:15 +0100

    Remove build files

--- build/antlr4cpp_generated_src/HexaC/HexaCParserBaseVisitor.cpp
+++
@@ -1,9 +0,0 @@
-
-// Generated from /Users/tbourvon/Drive/Projects/Programming/HexaC/grammar/HexaCParser.g4 by ANTLR 4.7.1
-
-
-#include "HexaCParserBaseVisitor.h"
-
-
-using namespace HexaC;
-
--- build/antlr4cpp_generated_src/HexaC/HexaCParserVisitor.cpp
+++
@@ -1,9 +0,0 @@
-
-// Generated from /Users/tbourvon/Drive/Projects/Programming/HexaC/grammar/HexaCParser.g4 by ANTLR 4.7.1
-
-
-#include "HexaCParserVisitor.h"
-
-
-using namespace HexaC;
-
